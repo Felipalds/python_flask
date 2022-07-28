bf5597c874dc370d1cbdc6562b499b773ee88dee
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2022-07-28 16:15:23 -0300

    actualize

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("joy.html")
+
+@app.route("/", methods=['POST'])
+def submit():
+    command = request.get_json()
+    print(command)
+    return "Hello"
